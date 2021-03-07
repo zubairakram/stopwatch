c95984e129ead81cbb0ac3c264cb14531fae5b38
Zubair Akram <zubair616@gmail.com>
2021-03-07 22:42:56 +0500

    some functionality added but incomplete

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,50 @@
 <script>
+    let hours = 0;
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+
+let interval = setInterval(start, 10);
+
+function start() {
+    milliseconds += 1;
+    if (milliseconds > 99) {
+        milliseconds = 0;
+        seconds +=1;
+        if (seconds > 59) {
+            seconds = 0;
+            minutes +=1;
+            if (minutes > 59) {
+                minutes = 0;
+                hours +=1;
+            }
+        }
+    }
+}
+
+
+function stop() {
+    clearInterval(interval);
+}
+
+function reset() {
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+    milliseconds = 0;
+
+}
 </script>
 
 <main>
-    <div>Screen</div>
-    <button>START</button>
-    <button>RESET</button>
+    <div><span>{ hours }</span>h</div>
+    <div><span>{ minutes }</span>m</div>
+    <div><span>{ seconds }</span>s</div>
+    <div><span>{ milliseconds }</span></div>
+
+    <button on:click={ start }>START</button>
+    <button on:click={ stop }>STOP</button>
+    <button on:click={ reset }>RESET</button>
 </main>
 
-<style>
-    main {
-        background-color: lightgreen;
-        padding: 0;
-        margin: 0;
-    }
-    div {
-        height: 100%;
-        width: 100%
-    }
-
-</style>
+<style></style>
