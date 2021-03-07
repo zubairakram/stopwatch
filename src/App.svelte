dd10231b8ea9099fdd70547ea136029c067cdbf1
Zubair Akram <zubair616@gmail.com>
2021-03-07 15:19:09 +0500

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<main>
+    <div>Screen</div>
+    <button>START</button>
+    <button>RESET</button>
+</main>
+
+<style>
+    main {
+        background-color: lightgreen;
+        padding: 0;
+        margin: 0;
+    }
+    div {
+        height: 100%;
+        width: 100%
+    }
+
+</style>
