778f4958e405548500caf2bdf8922b6fb62b6672
Zubair Akram <zubair616@gmail.com>
2021-03-21 20:55:58 +0500

    stop watch is working now

--- src/App.svelte
+++ src/App.svelte
@@ -1,49 +1,72 @@
 <script>
+    let isStarted = false;
     let hours = 0;
-let minutes = 0;
-let seconds = 0;
-let milliseconds = 0;
+    let minutes = 0;
+    let seconds = 0;
+    let milliseconds = 0;
+    let interval;
 
-let interval = setInterval(start, 10);
 
-function start() {
-    milliseconds += 1;
-    if (milliseconds > 99) {
-        milliseconds = 0;
-        seconds +=1;
-        if (seconds > 59) {
-            seconds = 0;
-            minutes +=1;
-            if (minutes > 59) {
-                minutes = 0;
-                hours +=1;
+    function counter() {
+        milliseconds += 1;
+        if (milliseconds > 99) {
+            milliseconds = 0;
+            seconds +=1;
+            if (seconds > 59) {
+                seconds = 0;
+                minutes +=1;
+                if (minutes > 59) {
+                    minutes = 0;
+                    hours +=1;
+                }
             }
         }
     }
-}
 
 
-function stop() {
-    clearInterval(interval);
-}
+    function start() {
 
-function reset() {
-    hours = 0;
-    minutes = 0;
-    seconds = 0;
-    milliseconds = 0;
+        if (!interval) {
+            isStarted = true;
+            interval = setInterval(counter, 10);
+        }
 
-}
+    }
+
+    function stop() {
+        if (interval) {
+            isStarted = false;
+            clearInterval(interval);
+            interval = null;
+        }
+    }
+
+    function reset() {
+        stop();
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
+        milliseconds = 0;
+
+    }
+
+    function format(n) {
+        if (n < 10) {
+            return `0${n}`;
+        } else {
+            return n;
+        }
+    }
 </script>
 
 <main>
-    <div><span>{ hours }</span>h</div>
-    <div><span>{ minutes }</span>m</div>
-    <div><span>{ seconds }</span>s</div>
-    <div><span>{ milliseconds }</span></div>
+    <div>{ format(hours) }h : { format(minutes) }m : { format(seconds) }s : { format(milliseconds) }</div>
 
-    <button on:click={ start }>START</button>
-    <button on:click={ stop }>STOP</button>
+    {#if isStarted}
+        <button on:click={ stop }>STOP</button>
+    {:else}
+        <button on:click={ start }>START</button>
+    {/if}
     <button on:click={ reset }>RESET</button>
 </main>
 
